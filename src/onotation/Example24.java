180b1bdf5c6deed6d22d0ae39ce5d9d0a795f759
D064288 <ruchi.dhiman@sap.com>
2022-04-17 15:00:35 +0530

    Add o notation example

--- src/onotation/Example24.java
+++ src/onotation/Example24.java
@@ -3,7 +3,7 @@ package onotation;
 import java.util.Arrays;
 
 public class Example24 {
-    int intersection(int[] a, int[] b) {
+    static int intersection(int[] a, int[] b) {
         int intersection = 0;
         Arrays.sort(b);
         for (int x : a) {
@@ -16,6 +16,9 @@ public class Example24 {
 
     // Time complexity: O(blogb + alogb)
     public static void main(String[] args) {
+        int[] a = new int[] {5,4,3,2,1};
+        int[] b = new int[] {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
+        System.out.println(intersection(a, b));
     }
 }
