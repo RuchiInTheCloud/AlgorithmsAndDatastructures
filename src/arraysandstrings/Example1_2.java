a63deaa4aa4e93ef026b4b3ce1b29470146e0c7e
D064288 <ruchi.dhiman@sap.com>
2022-06-01 15:20:20 +0530

    Description

--- src/arraysandstrings/Example1_2.java
+++ src/arraysandstrings/Example1_2.java
@@ -1,6 +1,21 @@
 package arraysandstrings;
 
-// Assume characters 'a' - 'z' are present in the string
+
+// Does a string have unique characters
+// Assumption: Assume characters 'a' - 'z' are present in the string
+// Example: abcdef, abba, String of size > 26
+// Brute force: In an integer keep track whether the character was seen
+// Brute force complexity: 1) Time O(N) 2) Space O(1)
+// Optimize: BCR
+// Walk Through:
+//              if string size > 26, return Not unique
+//              create integer = 0,
+//              for every character in string
+//                  if integer & toggled bit is true --> return Not unique
+//                  toggle the bit at the character index via |
+//              return Unique
+// Implement: ---
+// Test: ---
 public class Example1_2 {
     static boolean isUniqueChars(String str) {
         int checker = 0;
