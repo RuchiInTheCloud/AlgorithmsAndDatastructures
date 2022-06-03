14ab261bba25e1373b2fc8c127e652eeae0772de
D064288 <ruchi.dhiman@sap.com>
2022-06-03 07:41:52 +0530

    Add files

--- src/linkedlists/Example4_1.java
+++ src/linkedlists/Example4_1.java
@@ -1,4 +1,69 @@
 package linkedlists;
 
+import linkedlists.datastructures.LinkedList;
+
+//Partition a linked list around a value x such that elements < x appear on the left side and elements >= x on the right side
+//Example: I/P: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [x = 5] O/P: 3 -> 2 -> 1 -> 10 -> 5 -> 5 -> 8
+//Bruteforce: Traverse through the list and create two lists with elem < x, elem >= x
+//            Next join the two
+//Complexity: time: O(N)
+//Optimize: BCR
+//Walk through:
+//Help variable: remember start and end of both new lists to be able to join them
+//Node partition(Node head, int x)
+//Test: Be careful about circular references and overwriting original linked list
 public class Example4_1 {
+    static void partition(LinkedList<Integer> linkedList, int x) {
+        LinkedList<Integer>.Node beforeStart = null;
+        LinkedList<Integer>.Node beforeEnd = null;
+        LinkedList<Integer>.Node afterStart = null;
+        LinkedList<Integer>.Node afterEnd = null;
+
+        LinkedList<Integer>.Node current = linkedList.head;
+        while (current != null) {
+            LinkedList<Integer>.Node next = current.next;
+            current.next = null;
+            if (current.data < x) {
+                if (beforeStart == null) {
+                    beforeStart = current;
+                    beforeEnd = beforeStart;
+                } else {
+                    beforeEnd.next = current;
+                    beforeEnd = current;
+                }
+            } else {
+                if (afterStart == null) {
+                    afterStart = current;
+                    afterEnd = afterStart;
+                } else {
+                    afterEnd.next = current;
+                    afterEnd = current;
+                }
+            }
+            current = next;
+            current = current.next;
+        }
+
+        if (beforeStart == null) {
+            linkedList.head = afterStart;
+        }
+
+        beforeEnd.next = afterStart;
+        linkedList.head = beforeStart;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.appendToTail(3);
+        linkedList.appendToTail(5);
+        linkedList.appendToTail(8);
+        linkedList.appendToTail(5);
+        linkedList.appendToTail(10);
+        linkedList.appendToTail(2);
+        linkedList.appendToTail(1);
+
+        System.out.println("Initial List: " + linkedList);
+        partition(linkedList, 5);
+        System.out.println("After Partition: " + linkedList);
+    }
 }
--- src/linkedlists/Example4_2.java
+++ src/linkedlists/Example4_2.java
@@ -1,4 +1,58 @@
 package linkedlists;
 
+import linkedlists.datastructures.LinkedList;
+
+//Partition a linked list around a value x such that elements < x appear on the left side and elements >= x on the right side
+//Example: I/P: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [x = 5] O/P: 3 -> 2 -> 1 -> 10 -> 5 -> 5 -> 8
+//Bruteforce: Traverse through the list and create in place a new list such that elem < x are placed in front of head,
+// and elem >= x are placed after tail
+//Complexity: time: O(N)
+//Optimize: BCR
+//Walk through:
+//Help variable: head and tail pointing initially to linked list head, next to point to the next element to traverse to
+//void partition(Linkedlist list, int x)
+//Base case: No element in linked list
+//Base case: Only one element in linked list
+//Test:
 public class Example4_2 {
+    static void partition(LinkedList<Integer> linkedList, int x) {
+        if (linkedList.head == null) {
+            return;
+        }
+
+        LinkedList<Integer>.Node node = linkedList.head;
+        LinkedList<Integer>.Node next = node.next;
+        node.next = null;
+        LinkedList<Integer>.Node head = node;
+        LinkedList<Integer>.Node tail = node;
+
+        node = next;
+        while (node != null) {
+            next = node.next;
+            node.next = null;
+            if (node.data < x) {
+                node.next = head;
+                head = node;
+            }  else {
+                tail.next = node;
+                tail = node;
+            }
+            node = next;
+        }
+        linkedList.head = head;
+    }
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.appendToTail(3);
+        linkedList.appendToTail(5);
+        linkedList.appendToTail(8);
+        linkedList.appendToTail(5);
+        linkedList.appendToTail(10);
+        linkedList.appendToTail(2);
+        linkedList.appendToTail(1);
+
+        System.out.println("Initial List: " + linkedList);
+        partition(linkedList, 5);
+        System.out.println("After Partition: " + linkedList);
+    }
 }
