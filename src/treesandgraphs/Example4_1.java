4bf153992f9ac159c257343e450416c3bc0caa08
D064288 <ruchi.dhiman@sap.com>
2022-06-06 10:38:40 +0530

    Add files

--- src/treesandgraphs/Example4_1.java
+++ src/treesandgraphs/Example4_1.java
@@ -1,4 +1,67 @@
 package treesandgraphs;
 
+import treesandgraphs.datastructures.BinaryTree;
+
+//Check balanced: For every node in the tree, height of left and right subtree differs at most by 1
+//Brute force
+//
+//boolean checkBalanced(treenode node)
+//  if (node == null) return true;
+//  leftheight = height(node.left)
+//  rightheight = height(node.right)
+//  if (Math.abs(leftheight - rightheight) <= 1)
+//      return checkBalanced(node.left) && checkBalanced(node.right)
+//  else return false
+//
+//int height(treenode node)
+//  if (node == null) return -1;
+//  Math.max(height(node.left), height(node.right)) + 1;
+//
+// Complexity: ?????????
 public class Example4_1 {
+    private static boolean checkBalanced(BinaryTree<Integer> binaryTree) {
+        return checkBalanced(binaryTree.root);
+    }
+
+    private static boolean checkBalanced(BinaryTree.Node<Integer> node) {
+        if (node == null) {
+            return true;
+        }
+        int leftHeight = getHeight(node.left);
+        int rightHeight = getHeight(node.right);
+
+        if (Math.abs(leftHeight - rightHeight) <= 1) {
+            return checkBalanced(node.left) && checkBalanced(node.right);
+        } else {
+            return false;
+        }
+    }
+
+    private static int getHeight(BinaryTree.Node<Integer> node) {
+        if (node == null) {
+            return -1;
+        }
+        return Math.max(getHeight(node.left), getHeight(node.right)) + 1;
+    }
+
+    public static void main(String[] args) {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        BinaryTree.Node<Integer> node = new BinaryTree.Node<>(1);
+        BinaryTree.Node<Integer> subNode = new BinaryTree.Node<>(2);
+        node.left = subNode;
+        subNode = new BinaryTree.Node<>(3);
+        node.right = subNode;
+        binaryTree.root = node;
+
+        System.out.println("Is binary tree balanced? " + checkBalanced(binaryTree));
+
+        node = subNode;
+        subNode = new BinaryTree.Node<>(4);
+        node.right = subNode;
+        node = subNode;
+        subNode = new BinaryTree.Node<>(5);
+        node.right = subNode;
+
+        System.out.println("Is binary tree balanced? " + checkBalanced(binaryTree));
+    }
 }
--- src/treesandgraphs/Example4_2.java
+++ src/treesandgraphs/Example4_2.java
@@ -1,4 +1,30 @@
 package treesandgraphs;
 
+import treesandgraphs.datastructures.BinaryTree;
+
 public class Example4_2 {
+    public static void main(String[] args) {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        BinaryTree.Node<Integer> node = new BinaryTree.Node<>(1);
+        BinaryTree.Node<Integer> subNode = new BinaryTree.Node<>(2);
+        node.left = subNode;
+        subNode = new BinaryTree.Node<>(3);
+        node.right = subNode;
+        binaryTree.root = node;
+
+        System.out.println("Is binary tree balanced? " + checkBalanced(binaryTree));
+
+        node = subNode;
+        subNode = new BinaryTree.Node<>(4);
+        node.right = subNode;
+        node = subNode;
+        subNode = new BinaryTree.Node<>(5);
+        node.right = subNode;
+
+        System.out.println("Is binary tree balanced? " + checkBalanced(binaryTree));
+    }
+
+    private static boolean checkBalanced(BinaryTree<Integer> binaryTree) {
+        return false;
+    }
 }
