dd290619735770286a8de0ca648f3c77067ab53d
D064288 <ruchi.dhiman@sap.com>
2022-06-07 16:31:03 +0530

    Add files

--- src/treesandgraphs/Example9_1.java
+++ src/treesandgraphs/Example9_1.java
@@ -1,7 +1,13 @@
 package treesandgraphs;
 
 //An array is traversed from left to right to create a BST
-//Given a BST, determine all possible arrays that could generate the BSTL
+//Given a BST, determine all possible arrays that could generate the BST
+//
+//As we iterate through the array, the first element would be placed at the root
+//The second either the left or right side of the root
+//The third either the left or right side of the root
+//
+//
 public class Example9_1 {
 
     public static void main(String[] args) {
