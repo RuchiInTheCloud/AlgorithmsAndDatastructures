14236936dd56ea6bf17146e8346d848657354827
D064288 <ruchi.dhiman@sap.com>
2022-06-07 08:49:50 +0530

    Add files

--- src/treesandgraphs/datastructures/Tree.java
+++ src/treesandgraphs/datastructures/Tree.java
@@ -1,4 +1,27 @@
 package treesandgraphs.datastructures;
 
-abstract public class Tree<T> {
+public class Tree<T> {
+    public class Node<T> {
+        public T data;
+
+        public Node<T> parent;
+        public Node<T>[] children;
+
+        public Node(T data) {
+            this.data = data;
+            children = new Node[N];
+        }
+
+        @Override
+        public String toString() {
+            return data.toString();
+        }
+    }
+
+    public BinaryTree.Node<T> root;
+    private int N;
+
+    public Tree(int N) {
+        this.N = N;
+    }
 }
--- src/treesandgraphs/datastructures/Trie.java
+++ src/treesandgraphs/datastructures/Trie.java
@@ -1,4 +1,7 @@
 package treesandgraphs.datastructures;
 
 public class Trie extends Tree<Character> {
+    public Trie(int N) {
+        super(N);
+    }
 }
