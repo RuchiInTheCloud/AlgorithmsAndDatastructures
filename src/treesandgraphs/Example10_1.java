767c96a08243eb9e0ece3542a797aea9c37e7e51
D064288 <ruchi.dhiman@sap.com>
2022-06-08 11:59:37 +0530

    Add Files

--- src/treesandgraphs/Example10_1.java
+++ src/treesandgraphs/Example10_1.java
@@ -1,7 +1,89 @@
 package treesandgraphs;
 
-public class Example10_1 {
-    public static void main(String[] args) {
+import arraysandstrings.datastructures.StringBuilder;
+import treesandgraphs.datastructures.BinaryTree;
 
+//T1 and T2 are two tree. T1 is much larger than T2.
+//Write a method to determine if T2 is contained in T1
+//
+//     3           2
+//   2          1     3
+// 1
+//
+//1. In order traversal: X1X2X3X
+//2. In order traversal: X1X2X3X
+//1. Pre order traversal: 321XXXX
+//2. Pre order traversal: 21XX3XX
+//
+//Create a preorder traversal string for T1 and T2
+//Check whether T2 is part of T1
+//
+//preOrderString(Treenode node, StringBuilder sb)
+//  if (node == null) sb.append(X) return
+//  sb.append(node.data)
+//  preOrderString(node.left)
+//  preOrderString(node.right)
+//
+//boolean isSubtree(Treenode node1, Treenode node2)
+//  Stringbuilder preOrderString1 = new Stringbuilder()
+//  Stringbuilder preOrderString2 = new Stringbuilder()
+//  preOrderString(node1, preOrderString1)
+//  preOrderString(node2, preOrderString2)
+//  return preOrderString1.toString().indexOf(preOrderString2.toString()) > -1
+//
+//O(N + M) in time complexity where N is the size of T1 and M the size of T2
+//O(N + M) in space complexity
+public class Example10_1 {
+    static void preOrderString(BinaryTree.Node<Integer> node, StringBuilder sb) {
+        if (node == null) {
+            sb.append("X");
+            return;
+        }
+        sb.append(node.data + "");
+        preOrderString(node.left, sb);
+        preOrderString(node.right, sb);
+    }
+
+    static boolean isSubtree(BinaryTree<Integer> tree1, BinaryTree<Integer> tree2) {
+        StringBuilder preOrderString1 = new StringBuilder();
+        StringBuilder preOrderString2 = new StringBuilder();
+        preOrderString(tree1.root, preOrderString1);
+        preOrderString(tree2.root, preOrderString2);
+
+        return preOrderString1.toString().contains(preOrderString2.toString());
+    }
+
+    public static void main(String[] args) {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+
+        BinaryTree.Node<Integer> node = new BinaryTree.Node<>(2);
+        BinaryTree.Node<Integer> subNode = new BinaryTree.Node<>(1);
+        node.left = subNode;
+        subNode.parent = node;
+        subNode = new BinaryTree.Node<>(3);
+        node.right = subNode;
+        subNode.parent = node;
+
+        binaryTree.root = node;
+
+        node = subNode;
+        subNode = new BinaryTree.Node<>(4);
+        node.right = subNode;
+        subNode.parent = node;
+        node = subNode;
+        subNode = new BinaryTree.Node<>(5);
+        node.right = subNode;
+        subNode.parent = node;
+
+        BinaryTree<Integer> subBinaryTree = new BinaryTree<>();
+
+        node = new BinaryTree.Node<>(4);
+        subNode = new BinaryTree.Node<>(5);
+        node.right = subNode;
+        subNode.parent = node;
+
+        subBinaryTree.root = node;
+
+        System.out.println("Is T2 part of T1: " + isSubtree(binaryTree, subBinaryTree));
     }
 }
--- src/treesandgraphs/Example10_2.java
+++ src/treesandgraphs/Example10_2.java
@@ -1,4 +1,88 @@
 package treesandgraphs;
 
+import treesandgraphs.datastructures.BinaryTree;
+
+//T1 and T2 are two tree. T1 is much larger than T2.
+//Write a method to determine if T2 is contained in T1
+//
+//if T2 is null it is included
+//Other wise isSubtree(node1, node2)
+//if (node1 == null) return false;
+//if (node1.data == node2.data && matchSubtree(node1, node2)
+//return true
+//isSubtree(node.left, node2) || isSubtree(node.right, node2)
+//
+//boolean matchSubtree(node1, node2)
+//node1 == null && node2 == null
+//  return true;
+//node1 == null or node2 == null
+// return false;
+//node1.data != node2.data
+// return false;
+//matchSubtree(node1.left, node2.left) && matchSubtree(node1.right, node2.right)
 public class Example10_2 {
+    private static boolean isSubtree(BinaryTree<Integer> tree1, BinaryTree<Integer> tree2) {
+        if (tree2.root == null) {
+            return true;
+        } else {
+            return isSubtreeHelper(tree1.root, tree2.root);
+        }
+    }
+
+    private static boolean isSubtreeHelper(BinaryTree.Node<Integer> node1, BinaryTree.Node<Integer> node2) {
+        if (node1 == null) {
+            return false;
+        }
+        if (node1.data.equals(node2.data) && matchSubtree(node1, node2)) {
+            return true;
+        }
+        return isSubtreeHelper(node1.left, node2) || isSubtreeHelper(node1.right, node2);
+    }
+
+    private static boolean matchSubtree(BinaryTree.Node<Integer> node1, BinaryTree.Node<Integer> node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+        if (!node1.data.equals(node2.data)) {
+            return false;
+        }
+        return matchSubtree(node1.left, node2.left) && matchSubtree(node1.right, node2.right);
+    }
+
+    public static void main(String[] args) {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+
+        BinaryTree.Node<Integer> node = new BinaryTree.Node<>(2);
+        BinaryTree.Node<Integer> subNode = new BinaryTree.Node<>(1);
+        node.left = subNode;
+        subNode.parent = node;
+        subNode = new BinaryTree.Node<>(3);
+        node.right = subNode;
+        subNode.parent = node;
+
+        binaryTree.root = node;
+
+        node = subNode;
+        subNode = new BinaryTree.Node<>(4);
+        node.right = subNode;
+        subNode.parent = node;
+        node = subNode;
+        subNode = new BinaryTree.Node<>(5);
+        node.right = subNode;
+        subNode.parent = node;
+
+        BinaryTree<Integer> subBinaryTree = new BinaryTree<>();
+
+        node = new BinaryTree.Node<>(4);
+        subNode = new BinaryTree.Node<>(5);
+        node.right = subNode;
+        subNode.parent = node;
+
+        subBinaryTree.root = node;
+
+        System.out.println("Is T2 part of T1: " + isSubtree(binaryTree, subBinaryTree));
+    }
 }
