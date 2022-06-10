501f7cb4d940761ef426926116a6872708152171
D064288 <ruchi.dhiman@sap.com>
2022-06-10 11:50:50 +0530

    Add Files

--- src/_7_bitmanipulation/Example6_1.java
+++ src/_7_bitmanipulation/Example6_1.java
@@ -1,4 +1,19 @@
 package _7_bitmanipulation;
 
+//How many bit need to be flipped so that a and b are equal
+//The bits that are unequal can be detected via ^ XOR
+//Next count the 1s in the XOR result
 public class Example6_1 {
+    private static int getDifferentBitCount(int a, int b) {
+        int x = a ^ b;
+        int count = 0;
+        for (int i = x; i != 0; i = i >>> 1) {
+            count = count + (i & 1);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getDifferentBitCount(8, 7));
+    }
 }
---
+++ src/_7_bitmanipulation/Example6_2.java
@@ -0,0 +1,13 @@
+package _7_bitmanipulation;
+
+//How many bit need to be flipped so that a and b are equal
+
+public class Example6_2 {
+    private static int getDifferentBitCount(int a, int b) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getDifferentBitCount(8, 7));
+    }
+}
