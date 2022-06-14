aeb1b545c2b9fb21d5378c4cfda0c5dd6bdd90f0
D064288 <ruchi.dhiman@sap.com>
2022-06-14 20:11:08 +0530

    Add File

--- src/_9_objectorienteddesign/example9_1/CircularArray.java
+++ src/_9_objectorienteddesign/example9_1/CircularArray.java
@@ -1,4 +1,54 @@
 package _9_objectorienteddesign.example9_1;
 
-public class CircularArray {
+import java.util.Iterator;
+
+public class CircularArray<T> implements Iterable<T> {
+    T[] elements;
+    int head = 0;
+
+    public CircularArray(int capacity) {
+        this.elements = (T[]) new Object[capacity];
+    }
+
+    public void rotate(int shiftRight) {
+        head = convert(shiftRight);
+    }
+
+    public T get(int i) {
+        if (i < 0 || i >= elements.length) {
+            throw new java.lang.IndexOutOfBoundsException("Index " + i + " is out of bounds");
+        }
+        return elements[convert(i)];
+    }
+
+    public void set(int index, T item) {
+        elements[convert(index)] = item;
+    }
+
+    private int convert(int index) {
+        if (index < 0) {
+            index += elements.length;
+        }
+        return (head + index) % elements.length;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new CircularArrayIterator();
+    }
+
+    private class CircularArrayIterator implements Iterator<T> {
+        int currentIndex = -1;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex < elements.length - 1;
+        }
+
+        @Override
+        public T next() {
+            currentIndex++;
+            return elements[convert(currentIndex)];
+        }
+    }
 }
--- src/_9_objectorienteddesign/example9_1/Question.java
+++ src/_9_objectorienteddesign/example9_1/Question.java
@@ -1,4 +1,23 @@
 package _9_objectorienteddesign.example9_1;
 
 public class Question {
+    public static void main(String[] args) {
+        int size = 10;
+        CircularArray<String> array = new CircularArray<String>(size);
+        for (int i = 0; i < size; i++) {
+            array.set(i, String.valueOf(i));
+        }
+
+        array.rotate(3);
+        for (int i = 0; i < size; i++) {
+            System.out.print(array.get(i) + " ");
+        }
+
+        System.out.println();
+
+        array.rotate(2);
+        for (String s : array) {
+            System.out.print(s + " ");
+        }
+    }
 }
