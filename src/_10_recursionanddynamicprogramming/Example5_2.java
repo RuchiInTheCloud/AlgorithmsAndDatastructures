5639b560c866253fc39ea9a3a9d4b0b0f24d132b
D064288 <ruchi.dhiman@sap.com>
2022-06-18 18:20:52 +0200

    Add files

--- src/_10_recursionanddynamicprogramming/Example5_2.java
+++ src/_10_recursionanddynamicprogramming/Example5_2.java
@@ -1,4 +1,44 @@
 package _10_recursionanddynamicprogramming;
 
+//Write a recursive function to multiply two positive integers without using * or /
+//You may use addition, subtraction or bitwise shifting but minimize the number of these operations
+//product (n,m) = product (n/2, m) + product (n/2, m)
+//
+//              5, 10
+//      2, 10         3, 10
+//1, 10          1, 10      2, 10
+//
+//O(2^(log s)) = O(s) time complexity?, space complexity O(log s)
 public class Example5_2 {
+    static int product(int n, int m) {
+        int smaller = n < m ? n : m;
+        int larger = n > m ? n : m;
+        int[] memo = new int[smaller + 1];
+        return productHelper(smaller, larger, memo);
+    }
+
+    private static int productHelper(int smaller, int larger, int[] memo) {
+        if (smaller == 0) {
+            return 0;
+        } else if (smaller == 1) {
+            return larger;
+        } else if (memo[smaller] > 0) {
+            return memo[smaller];
+        } else {
+            System.out.println("I was here. Smaller = " + smaller + ", Larger = " + larger);
+
+            int temp = smaller >> 1;
+            int halfProduct = productHelper(temp, larger, memo);
+            int otherHalfProduct = halfProduct;
+            if (smaller % 2 == 1) {
+                otherHalfProduct = productHelper(smaller - temp, larger, memo);
+            }
+            memo[smaller] = halfProduct + otherHalfProduct;
+            return memo[smaller];
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Product of 3 and 5: " + product(5, 10));
+    }
 }
--- src/_10_recursionanddynamicprogramming/Example5_3.java
+++ src/_10_recursionanddynamicprogramming/Example5_3.java
@@ -1,4 +1,38 @@
 package _10_recursionanddynamicprogramming;
 
+//Write a recursive function to multiply two positive integers without using * or /
+//You may use addition, subtraction or bitwise shifting but minimize the number of these operations
+//product (n,m) = product (n/2, m) + product (n/2, m)
+//
+//              5, 10
+//      2, 10
+//1, 10
+//
+//O(log s), space complexity O(log s)
 public class Example5_3 {
+    static int product(int n, int m) {
+        int smaller = n < m ? n : m;
+        int larger = n > m ? n : m;
+        return productHelper(smaller, larger);
+    }
+
+    private static int productHelper(int smaller, int larger) {
+        if (smaller == 0) {
+            return 0;
+        } else if (smaller == 1) {
+            return larger;
+        } else {
+            int half = smaller >> 1;
+            int halfProduct = productHelper(half, larger);
+            if (smaller % 2 == 1) {
+                return halfProduct + halfProduct + larger;
+            } else {
+                return halfProduct + halfProduct;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Product of 3 and 5: " + product(5, 10));
+    }
 }
