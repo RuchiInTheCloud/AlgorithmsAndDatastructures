227d2abf3af8a3c343ae7f219a489b0304f00d52
D064288 <ruchi.dhiman@sap.com>
2022-06-19 05:20:15 +0200

    Add files

--- src/_10_recursionanddynamicprogramming/Example7_1.java
+++ src/_10_recursionanddynamicprogramming/Example7_1.java
@@ -7,6 +7,17 @@ import _3_arraysandstrings.datastructures.ArrayList;
 //P(a0a1) = a1a0 + a0a1
 //P(a0a1a2) = a2a1a0 + a1a2a0 + a1a0a2 + a2a0a1 + a0a2a1 + a0a1a2
 //P(a0a1a2a3) = Insert a3 into every position of every string in P(a0a1a2)
+//
+//Time Complexity = O(n!), Space Complexity = O(n!) ????????? According to text book
+//
+//P(0) = 0
+//P(1) = 1
+//P(2) = 2 + 2 = 2 * 2
+//P(3) = 3 + 3 + 3 + 3 + 3 + 3 = 3 * 6
+//P(4) = 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 = 4 * 24
+//P(n) = n * n!
+//O(n) = P(0) + P(1) + P(2) +... P(n)
+//     = ??
 public class Example7_1 {
     private static ArrayList<String> permutation(String string) {
         if (string == null) {
--- src/_10_recursionanddynamicprogramming/Example7_2.java
+++ src/_10_recursionanddynamicprogramming/Example7_2.java
@@ -1,4 +1,17 @@
 package _10_recursionanddynamicprogramming;
 
+import _3_arraysandstrings.datastructures.ArrayList;
+
+//Permutations
+//
+//P(a0) = a0
+//P(a0a1) = a1a0 + a0a1
+//P(a0a1a2) = a2a1a0 + a1a2a0 + a1a0a2 + a2a0a1 + a0a2a1 + a0a1a2
+//P(a0a1a2a3) = Insert a3 into every position of every string in P(a0a1a2)
+//
 public class Example7_2 {
+    public static void main(String[] args) {
+        ArrayList<String> permutations = permutation("abcd");
+        System.out.println(permutations);
+    }
 }
