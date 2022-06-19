9c5838e0a93213035a5f8eb17062065458e07343
D064288 <ruchi.dhiman@sap.com>
2022-06-19 09:26:07 +0200

    Add files

--- src/_10_recursionanddynamicprogramming/Example9_2.java
+++ src/_10_recursionanddynamicprogramming/Example9_2.java
@@ -1,13 +1,47 @@
 package _10_recursionanddynamicprogramming;
 
+import _3_arraysandstrings.datastructures.ArrayList;
+
 import java.util.HashSet;
 
 //Input: 3
 //Output: ((())), (()()),(())(),()(()),()()()
 //
+//Keep track of left parentheses remaining and right parentheses remaining
+//If left > 0, add left parentheses
+//If right > left, add right parentheses
 public class Example9_2 {
+    private static ArrayList<String> parantheses(int num) {
+        ArrayList<String> resultList = new ArrayList<>();
+        char[] parenthesesArray = new char[2 * num];
+        parantheses(num, num, 0, parenthesesArray, resultList);
+        return resultList;
+    }
+
+    private static void parantheses(int leftRemaining, int rightRemaining, int index, char[] parenthesesArray,
+            ArrayList<String> resultList) {
+        if (leftRemaining < 0 || rightRemaining < 0) {
+            return;
+        }
+
+        if (leftRemaining == 0 && rightRemaining == 0) {
+            String parenthesesString = String.copyValueOf(parenthesesArray);
+            resultList.add(parenthesesString);
+        } else {
+            if (leftRemaining > 0) {
+                parenthesesArray[index] = '(';
+                parantheses(leftRemaining - 1, rightRemaining, index + 1, parenthesesArray, resultList);
+            }
+
+            if (rightRemaining > leftRemaining) {
+                parenthesesArray[index] = ')';
+                parantheses(leftRemaining, rightRemaining - 1, index + 1, parenthesesArray, resultList);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        HashSet<String> combinations = parantheses(3);
+        ArrayList<String> combinations = parantheses(3);
         System.out.println(combinations);
     }
 }
