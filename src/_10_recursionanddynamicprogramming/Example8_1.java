46aead159113676ef6026c41dcff0c086ccfde87
D064288 <ruchi.dhiman@sap.com>
2022-06-19 06:39:48 +0200

    Add files

--- src/_10_recursionanddynamicprogramming/Example8_1.java
+++ src/_10_recursionanddynamicprogramming/Example8_1.java
@@ -1,4 +1,12 @@
 package _10_recursionanddynamicprogramming;
 
+import _3_arraysandstrings.datastructures.ArrayList;
+
+//Write a method to compute permutations of a string whose characters are not unique
+//Create unique permutations
 public class Example8_1 {
+    public static void main(String[] args) {
+        ArrayList<String> permutations = permutation("abba");
+        System.out.println(permutations);
+    }
 }
