1ada0c77623e54e4c7f0da9d6435c345ff894433
D064288 <ruchi.dhiman@sap.com>
2022-06-19 08:58:31 +0200

    Add files

--- src/_10_recursionanddynamicprogramming/Example9_1.java
+++ src/_10_recursionanddynamicprogramming/Example9_1.java
@@ -1,4 +1,40 @@
 package _10_recursionanddynamicprogramming;
 
+import _3_arraysandstrings.datastructures.ArrayList;
+
+import java.util.HashSet;
+
+//Input: 3
+//Output: ((())), (()()),(())(),()(()),()()()
+//
+//P(0) = ""
+//P(1) = ()
+//P(2) = ()() (())
+//P(3) = ()()() (())() ()(()) (()()) ((()))
+//P(3) = Place () at the front and after every opening brace in every element of P(2)
 public class Example9_1 {
+    private static HashSet<String> parantheses(int num) {
+        HashSet<String> parentheses = new HashSet<>();
+        if (num == 0) {
+            parentheses.add("");
+        } else {
+            HashSet<String> subSolutions = parantheses(num - 1);
+            for (String solution : subSolutions) {
+                for (int i = 0; i < solution.length(); i++) {
+                    if (solution.charAt(i) == '(') {
+                        String before = solution.substring(0, i + 1);
+                        String after = solution.substring(i + 1);
+                        parentheses.add(before + "()" + after);
+                    }
+                }
+                parentheses.add("()" + solution);
+            }
+        }
+        return parentheses;
+    }
+
+    public static void main(String[] args) {
+        HashSet<String> combinations = parantheses(3);
+        System.out.println(combinations);
+    }
 }
