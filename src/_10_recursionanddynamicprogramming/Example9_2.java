287ab6c9fa9ae8ae80115674fe5b67fbac87597e
D064288 <ruchi.dhiman@sap.com>
2022-06-19 09:16:13 +0200

    Add files

--- src/_10_recursionanddynamicprogramming/Example9_2.java
+++ src/_10_recursionanddynamicprogramming/Example9_2.java
@@ -1,4 +1,13 @@
 package _10_recursionanddynamicprogramming;
 
+import java.util.HashSet;
+
+//Input: 3
+//Output: ((())), (()()),(())(),()(()),()()()
+//
 public class Example9_2 {
+    public static void main(String[] args) {
+        HashSet<String> combinations = parantheses(3);
+        System.out.println(combinations);
+    }
 }
