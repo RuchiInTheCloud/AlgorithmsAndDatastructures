15f157ec57d45e11721e263ecb76419edb6df9cc
D064288 <ruchi.dhiman@sap.com>
2022-06-19 17:46:28 +0200

    Add files

--- src/_10_recursionanddynamicprogramming/Example13_3.java
+++ src/_10_recursionanddynamicprogramming/Example13_3.java
@@ -1,6 +1,7 @@
 package _10_recursionanddynamicprogramming;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 
 public class Example13_3 {
@@ -29,6 +30,33 @@ public class Example13_3 {
             return o2.height - o1.height;
         }
     }
+
+    private static int createStack(ArrayList<Box> boxes) {
+        Collections.sort(boxes, new BoxComparator());
+        int[] maxHeightMap = new int[boxes.size()];
+        return createStack(boxes, null, 0, maxHeightMap);
+    }
+
+    private static int createStack(ArrayList<Box> boxes, Box bottom, int offset, int[] maxHeightMap) {
+        if (offset >= boxes.size()) {
+            return 0;
+        }
+        
+        Box newBottom = boxes.get(offset);
+
+        int heightWithBottom = 0;
+        if (newBottom.canBeAbove(bottom)) {
+            if (maxHeightMap[offset] == 0) {
+                maxHeightMap[offset] = createStack(boxes, newBottom, offset + 1, maxHeightMap);
+                maxHeightMap[offset] += newBottom.height;
+            }
+            heightWithBottom = maxHeightMap[offset];
+        }
+
+        int heightWithoutBottom = createStack(boxes, bottom, offset + 1, maxHeightMap);
+        return Math.max(heightWithoutBottom, heightWithBottom);
+    }
+
     public static void main(String[] args) {
         ArrayList<Box> boxes = new ArrayList<>();
         Box box;
