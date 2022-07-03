7cdd44fc5a871d0a53b81db054fdd30f9d78b67a
D064288 <ruchi.dhiman@sap.com>
2022-07-03 13:50:19 +0200

    Add file

--- src/_12_sortingandsearching/Example2_2.java
+++ src/_12_sortingandsearching/Example2_2.java
@@ -1,4 +1,45 @@
 package _12_sortingandsearching;
 
+import _3_arraysandstrings.datastructures.ArrayList;
+import _3_arraysandstrings.datastructures.HashTable;
+import _4_linkedlists.datastructures.LinkedList;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class Example2_2 {
+    private static String sortChars(String string) {
+        char[] content = string.toCharArray();
+        Arrays.sort(content);
+        return new String(content);
+    }
+
+    private static void sort(String[] strings) {
+        HashTable<String, ArrayList<String>> mapList = new HashTable<>();
+        for (String string : strings) {
+            String sortedString = sortChars(string);
+            ArrayList<String> list = mapList.get(sortedString);
+            if (list == null) {
+                list = new ArrayList<>();
+                mapList.put(sortedString, list);
+            }
+            list.add(string);
+        }
+
+        int currentIndex = 0;
+        for (String key : mapList.keySet()) {
+            ArrayList<String> list = mapList.get(key);
+            Iterator<String> iterator = list.iterator();
+            while (iterator.hasNext()) {
+                strings[currentIndex] = iterator.next();
+                currentIndex += 1;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] strings = {"abba", "abcd", "baba", "burp"};
+        sort(strings);
+        System.out.println(Arrays.toString(strings));
+    }
 }
--- src/_12_sortingandsearching/Example3_1.java
+++ src/_12_sortingandsearching/Example3_1.java
@@ -1,4 +1,46 @@
 package _12_sortingandsearching;
 
+//Search in a sorted array that has been rotated
+//O(log n) runtime if elements are unique, otherwise O(n) in
+// worst case since left and right sub arrays need to be searched
 public class Example3_1 {
+    private static int search(int[] a, int x, int low, int high) {
+        if (low > high) {
+            return -1;
+        }
+        int mid = (low + high) / 2;
+        if (a[mid] == x) {
+            return mid;
+        }
+
+        if (a[low] < a[mid]) {
+            if (a[low] <= x && x < a[mid]) {
+                return search(a, x, low, mid - 1);
+            } else {
+                return search(a, x, mid + 1, high);
+            }
+        } else if (a[low] > a[mid]) {
+            if (a[mid] < x && x <= a[high]) {
+                return search(a, x, mid + 1, high);
+            } else {
+                return search(a, x, low, mid - 1);
+            }
+        } else if (a[low] == a[mid]) {
+            if (a[high] != a[mid]) {
+                return search(a, x, mid + 1, high);
+            } else {
+                int index = search(a, x, low, mid - 1);
+                if (index == -1) {
+                    index = search(a, x, mid + 1, high);
+                }
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {10, 15, 20, 0, 5};
+        System.out.println(search(a, 5, 0, 4));
+    }
 }
