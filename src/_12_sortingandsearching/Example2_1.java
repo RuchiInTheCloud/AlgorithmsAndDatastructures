69c4ea8c76ee802e70856137dbd9b13ca80d87b9
D064288 <ruchi.dhiman@sap.com>
2022-07-03 11:34:05 +0200

    Add file

--- src/_12_sortingandsearching/Example2_1.java
+++ src/_12_sortingandsearching/Example2_1.java
@@ -1,4 +1,25 @@
 package _12_sortingandsearching;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
+//Group anagrams
 public class Example2_1 {
+    private static class AnagramComparator implements Comparator<String> {
+        private String sortChars(String string) {
+            char[] content = string.toCharArray();
+            Arrays.sort(content);
+            return new String(content);
+        }
+
+        @Override
+        public int compare(String string1, String string2) {
+            return sortChars(string1).compareTo(sortChars(string2));
+        }
+    }
+    public static void main(String[] args) {
+        String[] strings = {"abba","abcd", "baba"};
+        Arrays.sort(strings, new AnagramComparator());
+        System.out.println(Arrays.toString(strings));
+    }
 }
