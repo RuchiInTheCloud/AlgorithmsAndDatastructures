3f52239e2150232b1010f46c8f898d7cf6748e99
D064288 <ruchi.dhiman@sap.com>
2022-07-04 17:06:29 +0200

    Add file

--- src/_12_sortingandsearching/Example9_1.java
+++ src/_12_sortingandsearching/Example9_1.java
@@ -1,4 +1,33 @@
 package _12_sortingandsearching;
 
+//Given MXN matrix with sorted rows and columns. Write method to find element.
+//Bruteforce: O(MlogN). Execute binary search in every row.
+//15   20   40   85
+//20   35   80   95
+//30   55   95  105
+//40   80  100  120
+//
+//If start of column is greater than the value, then look to the left
+//If end of row is smaller than the value, then look below
 public class Example9_1 {
+    private static boolean findElement(int[][] matrix, int elem) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == elem) {
+                return true;
+            } else if (matrix[row][col] > elem) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{15, 20, 40, 85}, {20, 35, 80, 95}, {30, 55, 95, 105}, {40, 80, 100, 120}};
+        boolean isPresent = findElement(matrix, 55);
+        System.out.println(isPresent);
+    }
 }
--- src/_12_sortingandsearching/Example9_2.java
+++ src/_12_sortingandsearching/Example9_2.java
@@ -1,4 +1,17 @@
 package _12_sortingandsearching;
 
+//Given MXN matrix with sorted rows and columns. Write method to find element.
+//Bruteforce: O(MlogN). Execute binary search in every row.
+//15   20   40   85
+//20   35   80   95
+//30   55   95  105
+//40   80  100  120
+//
 public class Example9_2 {
+
+    public static void main(String[] args) {
+        int[][] matrix = {{15, 20, 40, 85}, {20, 35, 80, 95}, {30, 55, 95, 105}, {40, 80, 100, 120}};
+        boolean isPresent = findElement(matrix, 55);
+        System.out.println(isPresent);
+    }
 }
