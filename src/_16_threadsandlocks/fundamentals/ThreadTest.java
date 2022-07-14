a1d4b84bd2588acfdb1c42ff2e43fcdd62aed9c2
D064288 <ruchi.dhiman@sap.com>
2022-07-14 19:50:32 +0200

    Add file

--- src/_16_threadsandlocks/fundamentals/ThreadTest.java
+++ src/_16_threadsandlocks/fundamentals/ThreadTest.java
@@ -1,4 +1,34 @@
 package _16_threadsandlocks.fundamentals;
 
 public class ThreadTest {
+    private static class ThreadExample extends Thread {
+        public int count = 0;
+
+        @Override
+        public void run() {
+            System.out.println("Thread starting.");
+            try {
+                while (count < 5) {
+                    Thread.sleep(500);
+                    count++;
+                }
+            } catch (InterruptedException ex) {
+                System.out.println("Thread interrupted.");
+            }
+            System.out.println("Thread terminating.");
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadExample instance = new ThreadExample();
+        instance.start();
+
+        while (instance.count != 5) {
+            try {
+                Thread.sleep(250);
+            } catch (InterruptedException exc) {
+                exc.printStackTrace();
+            }
+        }
+    }
 }
