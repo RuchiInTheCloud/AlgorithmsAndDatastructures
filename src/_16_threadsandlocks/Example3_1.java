98b4fa4b4e883795fb3606572c5002195ffeb452
D064288 <ruchi.dhiman@sap.com>
2022-07-16 19:33:14 +0200

    Add file

--- src/_16_threadsandlocks/Example3_1.java
+++ src/_16_threadsandlocks/Example3_1.java
@@ -1,4 +1,58 @@
 package _16_threadsandlocks;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+//Running below code may lead to deadlock if every philosopher has a left chopstick and waiting for the right one
 public class Example3_1 {
+    private static class Chopstick {
+        private Lock lock;
+
+        public Chopstick() {
+            lock = new ReentrantLock();
+        }
+
+        public void pickUp() {
+            lock.lock();
+        }
+
+        public void putDown() {
+            lock.unlock();
+        }
+    }
+
+    private static class Philosopher extends Thread {
+        private int bites = 10;
+        private Chopstick left, right;
+
+        public Philosopher(Chopstick left, Chopstick right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        public void eat() {
+            pickUp();
+            chew();
+            putdown();
+        }
+
+        public void pickUp() {
+            left.pickUp();
+            right.pickUp();
+        }
+
+        public void chew() {
+        }
+
+        public void putdown() {
+            right.putDown();
+            left.putDown();
+        }
+
+        public void run() {
+            for (int i = 0; i < bites; i++) {
+                eat();
+            }
+        }
+    }
 }
--- src/_16_threadsandlocks/Example3_2.java
+++ src/_16_threadsandlocks/Example3_2.java
@@ -1,4 +1,58 @@
 package _16_threadsandlocks;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+//Put down left chopstick if right unavailable
 public class Example3_2 {
+    private static class Chopstick {
+        private Lock lock;
+
+        public Chopstick() {
+            lock = new ReentrantLock();
+        }
+
+        public void pickUp() {
+            lock.tryLock();
+        }
+
+        public void putDown() {
+            lock.unlock();
+        }
+    }
+
+    private static class Philosopher extends Thread {
+        private int bites = 10;
+        private Chopstick left, right;
+
+        public Philosopher(Chopstick left, Chopstick right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        public void eat() {
+            pickUp();
+            chew();
+            putdown();
+        }
+
+        public void pickUp() {
+            left.pickUp();
+            right.pickUp();
+        }
+
+        public void chew() {
+        }
+
+        public void putdown() {
+            right.putDown();
+            left.putDown();
+        }
+
+        public void run() {
+            for (int i = 0; i < bites; i++) {
+                eat();
+            }
+        }
+    }
 }
