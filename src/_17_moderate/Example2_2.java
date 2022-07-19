31d00f081e5b2ffa2118c8fb09cd0327c333c019
D064288 <ruchi.dhiman@sap.com>
2022-07-19 15:49:18 +0200

    Add file

--- src/_17_moderate/Example2_2.java
+++ src/_17_moderate/Example2_2.java
@@ -1,18 +1,11 @@
 package _17_moderate;
 
 //Design a method to find the frequency of occurences of any given word in a book
-public class Example2_1 {
+//What if we were running this algorithm multiple times
+public class Example2_2 {
     private static int getFrequency(String[] words, String word) {
-        word = word.trim().toLowerCase();
-        int frequency = 0;
-        for (String w : words) {
-            if (w.trim().toLowerCase().equals(word)) {
-                frequency++;
-            }
-        }
-        return frequency;
-    }
 
+    }
     public static void main(String[] args) {
         String[] words = {" Ruchi", "RUCHI ", "Ru chi", "xyz"};
         String word = "RuChi";
