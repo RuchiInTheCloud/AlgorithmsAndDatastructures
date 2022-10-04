bf302774df5ef9d9ad886b975b91e0ea0e9c5361
D064288 <ruchi.dhiman@sap.com>
2022-10-04 21:51:10 +0200

    Add file

--- src/_17_moderate/example19_1/Example19_1.java
+++ src/_17_moderate/example19_1/Example19_1.java
@@ -1,4 +1,44 @@
 package _17_moderate.example19_1;
 
+import java.util.ArrayList;
+
+/*
+Given integer matrix representing plot of land
+value at location represents height above sea level. 0 = indicated water
+Pond = region of water connected vertically horizontally and diagonally
+Compute the size of all ponds
+ */
 public class Example19_1 {
+    static ArrayList<Integer> computePondSizes(int[][] land) {
+        ArrayList<Integer> pondSizes = new ArrayList<>();
+        for (int r = 0; r < land.length; r++) {
+            for (int c = 0; c < land[r].length; c++) {
+                if (land[r][c] == 0) {
+                    int size = computeSize(land, r, c);
+                    pondSizes.add(size);
+                }
+            }
+        }
+        return pondSizes;
+    }
+
+    static int computeSize(int[][] land, int row, int column) {
+        if (row < 0 || column < 0 || row >= land.length || column >= land[row].length || land[row][column] != 0) {
+            return 0;
+        }
+        int size = 1;
+        land[row][column] = -1;
+        for (int dr = -1; dr <= 1; dr++) {
+            for (int dc = -1; dc <= 1; dc++) {
+                size += computeSize(land, row + dr, column + dc);
+            }
+        }
+        return size;
+    }
+
+    public static void main(String[] args) {
+        int[][] land = {{0, 2, 1, 0}, {0, 1, 0, 1}, {1, 1, 0, 1}, {0, 1, 0, 1}};
+        ArrayList<Integer> pondSizes = computePondSizes(land);
+        System.out.println(pondSizes);
+    }
 }
--- src/_17_moderate/example19_1/Example19_2.java
+++ src/_17_moderate/example19_1/Example19_2.java
@@ -1,4 +1,45 @@
 package _17_moderate.example19_1;
 
+import java.util.ArrayList;
+
+/*
+Given integer matrix representing plot of land
+value at location represents height above sea level. 0 = indicated water
+Pond = region of water connected vertically horizontally and diagonally
+Compute the size of all ponds using visited matrix
+ */
 public class Example19_2 {
+    static ArrayList<Integer> computePondSizes(int[][] land) {
+        ArrayList<Integer> pondSizes = new ArrayList<>();
+        int[][] visited = new int[land.length][land[0].length];
+        for (int r = 0; r < land.length; r++) {
+            for (int c = 0; c < land[r].length; c++) {
+                if (land[r][c] == 0) {
+                    int size = computeSize(land, visited, r, c);
+                    pondSizes.add(size);
+                }
+            }
+        }
+        return pondSizes;
+    }
+
+    static int computeSize(int[][] land, int[][] visited, int row, int column) {
+        if (row < 0 || column < 0 || row >= land.length || column >= land[row].length || visited[row][column] != 0) {
+            return 0;
+        }
+        int size = 1;
+        visited[row][column] = -1;
+        for (int dr = -1; dr <= 1; dr++) {
+            for (int dc = -1; dc <= 1; dc++) {
+                size += computeSize(land, visited, row + dr, column + dc);
+            }
+        }
+        return size;
+    }
+
+    public static void main(String[] args) {
+        int[][] land = {{0, 2, 1, 0}, {0, 1, 0, 1}, {1, 1, 0, 1}, {0, 1, 0, 1}};
+        ArrayList<Integer> pondSizes = computePondSizes(land);
+        System.out.println(pondSizes);
+    }
 }
