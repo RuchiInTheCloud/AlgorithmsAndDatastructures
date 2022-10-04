1b58d2d690c15a158d394d57cc8ed7eda4a5f3b1
D064288 <ruchi.dhiman@sap.com>
2022-10-04 19:06:09 +0200

    Add file

--- src/_17_moderate/Example17_1.java
+++ src/_17_moderate/Example17_1.java
@@ -1,5 +1,28 @@
 package _17_moderate;
 
+/*
+Given array of positive and negative integers. Find contiguous sequence with the largest sum
+E.g. 2, -8, 3, -2, 4, -10
+Build contiguous sum until it turns negative in which case reset
+Contiguous subsequence of sum 0 is better than the negative sum
+ */
 public class Example17_1 {
+    static int getMax(int[] a) {
+        int maxSum = 0;
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+            if (maxSum < sum) {
+                maxSum = sum;
+            } else if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
 
+    public static void main(String[] args) {
+        int[] a = {2, -8, 3, -2, 4, -10};
+        System.out.println(getMax(a));
+    }
 }
