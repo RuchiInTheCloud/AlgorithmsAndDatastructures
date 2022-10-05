5848041824031ed8fd0086679cbcce0b0b5877bc
D064288 <ruchi.dhiman@sap.com>
2022-10-05 12:30:59 +0200

    Add file

--- src/_17_moderate/example21_1/Example21_2.java
+++ src/_17_moderate/example21_1/Example21_2.java
@@ -7,12 +7,31 @@ Given two arrays of integers: Find a pair of values that you can swap to give th
 We are looking for values a, b such that
 sumA - a + b = sumB - b + a
 (a - b) = (sumA - sumB) / 2
-sumA - sumB must be even otherwise there is no result
+sumA - sumB must be even otherwise there is no result. Use Integer to save (sumA-sumB)/2, null to indicate error
 Complexity O(AB)
-
  */
 public class Example21_2 {
     static int[] findSwapValues(int[] array1, int[] array2) {
+        Integer target = getTarget(array1, array2);
+        if (target == null) {
+            return null;
+        }
+        for (int one : array1) {
+            for (int two : array2) {
+                if (one - two == target) {
+                    return new int[]{one, two};
+                }
+            }
+        }
+        return null;
+    }
+
+    static Integer getTarget(int[] array1, int[] array2) {
+        int sum1 = Arrays.stream(array1).sum();
+        int sum2 = Arrays.stream(array2).sum();
+        if ((sum1 - sum2) % 2 != 0)
+            return null;
+        return (sum1 - sum2) / 2;
     }
 
     public static void main(String[] args) {
---
+++ src/_17_moderate/example22_1/Example22_1.java
@@ -0,0 +1,4 @@
+package _17_moderate.example22_1;
+
+public class Example22_1 {
+}
