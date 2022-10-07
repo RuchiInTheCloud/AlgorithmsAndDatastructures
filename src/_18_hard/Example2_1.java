b6c6b32f17f7417fbc094bc61936ef85fdb9a6ad
D064288 <ruchi.dhiman@sap.com>
2022-10-07 19:05:25 +0200

    Add file

--- src/_18_hard/Example2_1.java
+++
@@ -1,4 +0,0 @@
-package _18_hard;
-
-public class Example2_1 {
-}
---
+++ src/_18_hard/example2_1/Example2_2.java
@@ -0,0 +1,29 @@
+package _18_hard.example2_1;
+
+import java.util.Arrays;
+
+/*
+Shuffle a deck of 52 cards with every permutation being equally likely
+Based on base case of n - 1 elements, Generate a combination and swap the n-th element with an element in the array
+ */
+public class Example2_2 {
+    static int rand(int higher) {
+        return (int) (Math.random() * (higher + 1));
+    }
+
+    static int[] shuffleArrayRecursively(int[] cards) {
+        for (int i = 0; i < cards.length; i++) {
+            int k = rand(i);
+
+            int temp = cards[k];
+            cards[k] = cards[i];
+            cards[i] = temp;
+        }
+        return cards;
+    }
+
+    public static void main(String[] args) {
+        int[] cards = new int[]{1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(shuffleArrayRecursively(cards)));
+    }
+}
