17443cc95e5d96962ddd3c12f88e6cdfdf19ddbd
D064288 <ruchi.dhiman@sap.com>
2022-10-07 17:48:22 +0200

    Add file

--- src/_18_hard/Example1_1.java
+++ src/_18_hard/Example1_1.java
@@ -1,4 +1,20 @@
 package _18_hard;
 
+/*
+Add without plus
+ */
 public class Example1_1 {
+    static int add(int a, int b) {
+        while (b != 0) {
+            int sum = a ^ b;
+            int carry = (a & b) << 1;
+            a = sum;
+            b = carry;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(759, 674));
+    }
 }
